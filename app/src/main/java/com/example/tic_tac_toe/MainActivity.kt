19c0d65fee61a5b6aab3ee08b08f6460c134ab1e
dvnfirefox <134988327+dvnfirefox@users.noreply.github.com>
2024-08-19 20:45:26 -0400

    Implement grid implement draw condition implement win condition

--- app/src/main/java/com/example/tic_tac_toe/MainActivity.kt
+++ app/src/main/java/com/example/tic_tac_toe/MainActivity.kt
@@ -20,8 +20,9 @@ class MainActivity : AppCompatActivity() {
     private lateinit var case7: Button
     private lateinit var case8: Button
     private lateinit var start: Button
-    private var grid = arrayOf(0,0,0,0,0,0,0,0,0)
+    private val grid = Array(3) { Array(3) { 0 } }
     private var lastPlay = "X"
+    private var win = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -43,60 +44,119 @@ class MainActivity : AppCompatActivity() {
         start = findViewById(R.id.start)
 
         case0.setOnClickListener {
-            gridClick(case0,0)
+            gridClick(case0,0,0)
         }
         case1.setOnClickListener {
-            gridClick(case1,1)
+            gridClick(case1,0,1)
         }
         case2.setOnClickListener {
-            gridClick(case2,2)
+            gridClick(case2,0,2)
         }
         case3.setOnClickListener {
-            gridClick(case3,3)
+            gridClick(case3,1,0)
         }
         case4.setOnClickListener {
-            gridClick(case4,4)
+            gridClick(case4,1,1)
         }
         case5.setOnClickListener {
-            gridClick(case5,5)
+            gridClick(case5,1,2)
         }
         case6.setOnClickListener {
-            gridClick(case6,6)
+            gridClick(case6,2,0)
         }
         case7.setOnClickListener {
-            gridClick(case7,7)
+            gridClick(case7,2,1)
         }
         case8.setOnClickListener {
-            gridClick(case8,8)
+            gridClick(case8,2,2)
         }
         start.setOnClickListener {
-            grid = arrayOf(0,0,0,0,0,0,0,0,0)
-            case0.setText("")
-            case1.setText("")
-            case2.setText("")
-            case3.setText("")
-            case4.setText("")
-            case5.setText("")
-            case6.setText("")
-            case7.setText("")
-            case8.setText("")
-
+            reset()
         }
-
     }
-    private fun gridClick(button: Button, index: Int) {
-        if(grid[index] == 0 && lastPlay == "X" ) {
+    private fun reset(){
+        for (row in grid.indices) {
+            for (col in grid[row].indices) {
+                grid[row][col] = 0
+            }
+        }
+        case0.setText("")
+        case1.setText("")
+        case2.setText("")
+        case3.setText("")
+        case4.setText("")
+        case5.setText("")
+        case6.setText("")
+        case7.setText("")
+        case8.setText("")
+        win = false
+    }
+    private fun gridClick(button: Button, index0: Int, index1: Int) {
+        if (grid[index0][index1] != 0 || win) {
+            return
+        }
+        if(lastPlay == "X" ) {
             button.setText("O")
-            grid[index] = 1
+            grid[index0][index1] = 1
             lastPlay = "O"
-        }else if(grid[index] == 0 && lastPlay == "O" ) {
+        }else{
             button.setText("X")
-            grid[index] = 1
+            grid[index0][index1] = -1
             lastPlay = "X"
-
         }
-
+        drawCheck()
+        winCheck()
     }
-
+    private fun drawCheck (){
+        var gridFull = 0
+        for (row in grid.indices) {
+            for (col in grid[row].indices) {
+                if(grid[row][col] != 0){
+                    gridFull ++
+                }
+            }
+        }
+        if(gridFull == 9){
+            Toast.makeText(this@MainActivity, "DRAW", Toast.LENGTH_SHORT).show()
+        }
+    }
+    private fun winCheck(){
+        for (row in grid.indices) {
+            var count = 0
+            for (col in grid[row].indices) {
+                count = grid[row][col] + count
+            }
+            countCheck(count)
+        }
+        for (col in grid.indices) {
+            var count = 0
+            for (row in grid[col].indices) {
+                count = grid[row][col] + count
+            }
+            countCheck(count)
+        }
+        var count = 0
+        for(i in 2 downTo 0){
+            count = count + grid[i][i]
+        }
+        countCheck(count)
+        count = 0
+        for(i in 2 downTo 0){
+            count = count + grid[i][2-i]
+        }
+        countCheck(count)
+    }
+    private fun countCheck(count: Int){
+        if(count == -3){
+            Toast.makeText(this@MainActivity, "WIN Player 1", Toast.LENGTH_SHORT).show()
+            win = true
+            return
+        }
+        if(count == 3){
+            Toast.makeText(this@MainActivity, "WIN Player 2", Toast.LENGTH_SHORT).show()
+            win = true
+            return
+        }
+    }
 
 }
